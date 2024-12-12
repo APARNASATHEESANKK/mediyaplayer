fbd88830215a2fbc8c8421937377f118d6fb2895
APARNASATHEESANKK <aparnakk9988@gmail.com>
2024-12-12 14:36:31 +0530

    mediaplayer server

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer =require('json-server')
+
+const mpserver=jsonServer.create()
+
+const middleware=jsonServer.defaults()
+const route=jsonServer.router('db.jason')
+
+const PORT=3000 || process.env.PORT
+
+mpserver.use(middleware)
+mpserver.use(route)
+
+mpserver.listen(PORT,()=>{
+    console.log(`media player server running at ${PORT} and waiting for client request`);
+})
